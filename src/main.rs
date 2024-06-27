9481e36178d775afec95997a218a178efe5894d4
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-06-27 12:49:44 +0530

    tests for hash function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use kupyna_512;
 
 fn main() {
-    let message = b"".to_vec();
+    let message = b"Hello, World!".to_vec();
     let _message_length = 0;
 
     let hash_code = kupyna_512::hash(message, None).unwrap();
